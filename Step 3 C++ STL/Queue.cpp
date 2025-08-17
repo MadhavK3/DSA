c1210c0481d51e06c97c1e36e7d5c0813a56c934
MadhavK3 <madhavkhobare@gmail.com>
2025-08-17 17:07:55 +0530

    Queue

--- Step 3 C++ STL/Queue.cpp
+++ Step 3 C++ STL/Queue.cpp
@@ -11,7 +11,21 @@ int main(){
     q.emplace(30);  // Faster insertion than push (no copy involved)
     cout << "Elements in queue after adding: ";
     // Displaying elements in the queue
-    
+    while(!q.empty()) {
+        cout << q.front() << " ";  // front() returns the first element added
+        q.pop();  // pop() removes the first element
+    }
+
+    // no top() function in queue like stack but we can use front() to get the first element
+    // q.back() is also available to get the last element added
+    cout << "\nQueue is empty now-";  // Now queue is empty
+    cout << q.size() << endl;  // size will be 0 now
+
+    // All other functions like size, empty, swap, etc. are also available just like vector
+    // Accessing elements
+    // cout << "First element: " << q.front() << endl;  // front() returns the first element added
+    // cout << "Queue size: " << q.size() << endl;  // size
+
     return 0;
 
 }
