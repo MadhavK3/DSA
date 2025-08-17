8332666513d906c5a0c123cdda5b3ce84734193c
MadhavK3 <madhavkhobare@gmail.com>
2025-08-17 17:34:45 +0530

    Map Syntax

---
+++ Step 3 C++ STL/Map.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    cout<<"Using Map in C++ STL  key is unique and sorted and other element can be duplicate eg clg diff roll no but same names" << endl;
+    
+    return 0;
+
+}
