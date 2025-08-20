661e233a4f4308504b55dc1256322d20d38b5755
MadhavK3 <madhavkhobare@gmail.com>
2025-08-20 13:13:15 +0530

    Introduction to Recursion

---
+++ Step 5 Recursion/Introduction to Recursion.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// This program demonstrates infinite recursion.
+// Rule: A function that calls itself without a base case will lead to infinite recursion.
+void print(){
+    cout<<1<<" ";
+    print();
+}
+
+// this is finite with base condition
+void print2(int n){
+    
+    cout<<2<<" ";
+    n++;
+    if(n < 5) {print2(n);}
+
+
+    
+}
+int main(){
+int count = 0;
+    print2(count);
+    // This will cause a stack overflow due to infinite recursion.
+    return 0;
+
+}
